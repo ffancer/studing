2158829248d49ba8d9a7cd6f63be58ca462b2547
Paul <56205086+ffancer@users.noreply.github.com>
2021-07-29 08:21:02 +0300

    Create 7 kyu Alphabet war.py

---
+++ 7 kyu Alphabet war.py
@@ -0,0 +1,72 @@
+"""
+https://www.codewars.com/kata/59377c53e66267c8f6000027/train/python
+
+Introduction
+
+There is a war and nobody knows - the alphabet war!
+There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+Task
+
+Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+The left side letters and their power:
+
+ w - 4
+ p - 3
+ b - 2
+ s - 1
+
+The right side letters and their power:
+
+ m - 4
+ q - 3
+ d - 2
+ z - 1
+
+The other letters don't have power and are only victims.
+Example
+
+AlphabetWar("z");        //=> Right side wins!
+AlphabetWar("zdqmwpbs"); //=> Let's fight again!
+AlphabetWar("zzzzs");    //=> Right side wins!
+AlphabetWar("wwwwwwz");  //=> Left side wins!
+"""
+
+
+def alphabet_war(fight):
+    score_left = 0
+    score_right = 0
+
+    for i in fight[:len(fight)//2]:
+        if i == 'w':
+            score_left += 4
+        elif i == 'p':
+            score_left += 3
+        elif i == 'b':
+            score_left += 2
+        elif i == 's':
+            score_left += 1
+
+    for j in fight[len(fight)//2:]:
+        if j == 'm':
+            score_right += 4
+        elif j == 'q':
+            score_right += 3
+        elif j == 'd':
+            score_right += 2
+        elif j == 'z':
+            score_right += 1
+
+    if score_left == score_right:
+        return "Let's fight again!"
+    elif score_left > score_right:
+        return 'Left side wins!'
+    return 'Right side wins!'
+
+
+
+print(alphabet_war("z"), "Right side wins!")
+print(alphabet_war("zdqmwpbs"), "Let's fight again!")
+print(alphabet_war("wq"), "Left side wins!")
+print(alphabet_war("zzzzs"), "Right side wins!")
+print(alphabet_war("wwwwww"), "Left side wins!")
