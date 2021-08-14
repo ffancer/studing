f18f2ab1a15bab26b2cef8782c5ca2f8fc5df25a
Paul <56205086+ffancer@users.noreply.github.com>
2021-08-14 10:28:08 +0300

    Create 7 kyu Strings: swap vowels' case.py

---
+++ 7 kyu Strings: swap vowels' case.py
@@ -0,0 +1,31 @@
+"""
+https://www.codewars.com/kata/5803c0c6ab6c20a06f000026/train/python
+
+pecial thanks to SteffenVogel_79 for the idea.
+
+Challenge:
+
+Given a string, return a copy of the string with the vowels' case swapped.
+
+For this kata, assume that vowels are in the set "aeouiAEOUI".
+
+Example: Given a string "C is alive!", your function should return "C Is AlIvE!"
+
+Addendum: Your solution is only required to work for the ASCII character set.
+
+Please make sure you only swap cases for the vowels.
+"""
+
+
+def swap_vowel_case(st): 
+    s = ''
+    
+    for i in st:
+        if i in 'aeoiu':
+            s += i.upper()
+        elif i in 'AEOUI':
+            s += i.lower()
+        else:
+            s += i
+            
+    return s
