6c05ab7c3d0a99a007ada2903ef35f62ebac51f6
Paul <56205086+ffancer@users.noreply.github.com>
2021-08-17 10:21:35 +0300

    Create 6 kyu Sum of a Sequence [Hard-Core Version].py

---
+++ 6 kyu Sum of a Sequence [Hard-Core Version].py
@@ -0,0 +1,25 @@
+"""
+https://www.codewars.com/kata/587a88a208236efe8500008b/train/python
+
+As the title suggests, this is the hard-core version of another neat kata.
+
+The task is simple to explain: simply sum all the numbers from the first parameter being the beginning to the second parameter being the upper limit (possibly included), going in steps expressed by the third parameter:
+
+sequence_sum(2, 2, 2) # 2
+sequence_sum(2, 6, 2) # 12 (= 2 + 4 + 6)
+sequence_sum(1, 5, 1) # (= 1 + 2 + 3 + 4 + 5)
+sequence_sum(1, 5, 3) # 5 (= 1 + 4)
+
+If it is an impossible sequence (with the beginning being larger the end and a positive step or the other way around), just return 0. See the provided test cases for further examples :)
+
+Note: differing from the other base kata, much larger ranges are going to be tested, so you should hope to get your algo optimized and to avoid brute-forcing your way through the solution.
+"""
+
+
+def sequence_sum(begin_number, end_number, step):
+    if begin_number > end_number and step > 0 or begin_number < end_number and step < 0:
+        return 0
+
+    formula = (end_number - begin_number) // step + 1
+
+    return (2 * begin_number + step * (formula - 1)) * formula // 2
