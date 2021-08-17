fadd632959eaa9816b6c3ddef221d782e82d462c
Paul <56205086+ffancer@users.noreply.github.com>
2021-08-17 13:21:12 +0300

    Create 7 kyu Make a function that does arithmetic!.py

---
+++ 7 kyu Make a function that does arithmetic!.py
@@ -0,0 +1,53 @@
+"""
+https://www.codewars.com/kata/583f158ea20cfcbeb400000a/train/python
+
+Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+The four operators are "add", "subtract", "divide", "multiply".
+
+A few examples:
+
+arithmetic(5, 2, "add")      => returns 7
+arithmetic(5, 2, "subtract") => returns 3
+arithmetic(5, 2, "multiply") => returns 10
+arithmetic(5, 2, "divide")   => returns 2.5
+
+ArithmeticFunction.arithmetic(5, 2, "add")      => returns 7
+ArithmeticFunction.arithmetic(5, 2, "subtract") => returns 3
+ArithmeticFunction.arithmetic(5, 2, "multiply") => returns 10
+ArithmeticFunction.arithmetic(5, 2, "divide")   => returns 2
+
+Try to do it without using if statements!
+"""
+
+
+def arithmetic(a, b, operator):
+    def add():
+        return a + b
+    
+    
+    def subtract():
+        return a - b
+
+
+    def multiply():
+        return a * b
+
+
+    def divide():
+        return a / b
+    
+    
+    method = {
+    "add": add,
+    "subtract": subtract,
+    "multiply": multiply,
+    "divide": divide
+    }
+    
+    return method[operator]()
+  
+  
+  # very proud of myself, coz no 'if-else' 
